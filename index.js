b5801bc998f845dbafe0fdcba7c9c5a8a5c9ba83
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2020-05-03 13:02:19 -0400

    Installed dotenv, added (.env) connection string

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose"); //to connect to mongoDB
+
+dotenv.config(); //initiate env variables
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION_STRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to the database!");
+  }
+);
 
 //Import routes
 const authRoute = require("./routes/auth");
