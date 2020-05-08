b7783daeaad067b41213cfcc2f57172173927663
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2020-05-08 10:26:00 -0400

    Validation working

--- models/user.js
+++ models/user.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 
 //*** min and max not working ***/
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+      max: 1024,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 8,
-    max: 1024,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+//error
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).required(),
+});
+
 //Auth routes (/api/user/...)
 router.post("/register", async (req, res) => {
   //res.send("Register page"); //send/display on page
+
+  //Validate before creating a User
+  const { error } = schema.validate(req.body); //validate body with schema, returns object
+  if (error) {
+    console.log("error", error);
+    return res.status(400).send(error.details[0].message);
+  }
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
